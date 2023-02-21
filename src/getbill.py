8425c8e5fc1e2d17beb6534ff37d48f440c117e9
Paolo Vacarizas <pavacarizas@gmail.com>
2023-02-21 13:51:49 +0100

    fix EAN extraction, update queries

--- src/getbill.py
+++ src/getbill.py
@@ -2,7 +2,7 @@ from PyPDF2 import PdfReader
 from settings import *
 import pygsheets, re
 
-month = "FEB"
+month = "MAR"
 reader = PdfReader(DOCS_ROOT + month + ".pdf")
 number_of_pages = len(reader.pages)
 text = ""
@@ -18,16 +18,21 @@ for i in range(number_of_pages):
 
 query = re.compile("".join(BILL_QUERY))
 
+ean_el = re.search(r"(?P<EAN_el>(?<=\nEAN[\W]{2}elektriciteit\n:\n)[\d]+)",text).group(0)
+ean_gas = re.search(r"(?P<EAN_gas>(?<=\nEAN[\W]{2}gas\n:\n)[\d]+)",text).group(0)
+print(ean_el)
+print(ean_gas)
 q = query.search(text)
 print(q.group('present_period'))
+
 # print(month)
 # print(invoice_num)
 
-gc = pygsheets.authorize(service_file=GCREDS)
-sh = gc.open(GSPREADSHEET)
-# print(sh.sheets)
-
-wks = sh.worksheet('title','Copy of '+ month)
-
-wks.update_value('B4',q.group('present_period'))
-print('sheets updated!')
+# gc = pygsheets.authorize(service_file=GCREDS)
+# sh = gc.open(GSPREADSHEET)
+# # print(sh.sheets)
+#
+# wks = sh.worksheet('title','Copy of '+ month)
+#
+# wks.update_value('B4',q.group('present_period'))
+# print('sheets updated!')
