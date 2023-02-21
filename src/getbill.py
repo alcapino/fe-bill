9e8ce1a300af03246767da8ec50f646335b9d0a6
Paolo Vacarizas <pavacarizas@gmail.com>
2023-02-21 18:08:53 +0100

    completed queries, refactor strings, started writing to sheet

--- src/getbill.py
+++ src/getbill.py
@@ -2,10 +2,10 @@ from PyPDF2 import PdfReader
 from settings import *
 import pygsheets, re
 
-month = "MAR"
-reader = PdfReader(DOCS_ROOT + month + ".pdf")
+month = 'FEB'
+reader = PdfReader(f'{DOCS_ROOT}{month}.pdf')
 number_of_pages = len(reader.pages)
-text = ""
+text = ''
 for i in range(number_of_pages):
     page = reader.pages[i]
     text += page.extract_text()
@@ -16,23 +16,28 @@ for i in range(number_of_pages):
 # f.close()
 # print("temp file written!")
 
-query = re.compile("".join(BILL_QUERY))
 
-ean_el = re.search(r"(?P<EAN_el>(?<=\nEAN[\W]{2}elektriciteit\n:\n)[\d]+)",text).group(0)
-ean_gas = re.search(r"(?P<EAN_gas>(?<=\nEAN[\W]{2}gas\n:\n)[\d]+)",text).group(0)
-print(ean_el)
-print(ean_gas)
-q = query.search(text)
-print(q.group('present_period'))
+ean_el = re.search(EAN_ELECTRIC,text).group(0)
+ean_gas = re.search(EAN_GAS,text).group(0)
+cover_extra_month = re.search(COVER_EXTRA,text)
+refund_extra_month = re.search(REFUND_EXTRA,text)
+bill_values = re.search(BILL_QUERY,text)
+print(f'Bill for {month} has been retrieved. Generating report...')
 
-# print(month)
-# print(invoice_num)
+gc = pygsheets.authorize(service_file=GCREDS)
+sh = gc.open(GSPREADSHEET)
+# print(sh.sheets)
 
-# gc = pygsheets.authorize(service_file=GCREDS)
-# sh = gc.open(GSPREADSHEET)
-# # print(sh.sheets)
-#
-# wks = sh.worksheet('title','Copy of '+ month)
-#
-# wks.update_value('B4',q.group('present_period'))
-# print('sheets updated!')
+wks = sh.worksheet('title',f'Copy of {month}')
+if bill_values is not None:
+    wks.update_value('B2',bill_values.group('invoice_num'))
+    wks.update_value('B3', bill_values.group('due_date'))
+    wks.update_value('B4',bill_values.group('present_period'))
+    wks.update_value('F2', bill_values.group('incl_BWT'))
+    wks.update_value('F3', bill_values.group('invoice_amt'))
+    # wks.update_value('B2', bill_values.group('____SAMPLE___'))
+    # wks.update_value('B2', bill_values.group('____SAMPLE___'))
+else:
+    print('Unrecognized document. Script will exit.')
+    exit()
+print(f'Sheet {month} updated!')
--- src/settings.py
+++ src/settings.py
@@ -1,37 +1,45 @@
-DOCS_ROOT = "../docs/"
-GCREDS = "./creds.json"
-GSPREADSHEET = "Utilities Dashboard"
-BILL_QUERY = [
-    'Klantnummer:\n',
-    '(?P<customer_num>\d+)\nFactuurnummer:\n',
-    '(?P<invoice_num>\d+)\nVervaldatum:\n',
-    '(?P<due_date>[\d\-]+)[\s\S]+Termijnfactuur\s{3}',
-    '(?P<month>\w*)[\s\S]+Vaste leveringskosten\n',
-    '(?P<present_period>[\d]{2}\-[\d]{2} t\/m [\d]{2}\-[\d]{2})[\W]{3}',
-    '(?P<del_cost_novat>(?<=€\n)[\d\,]+)[\W]{3}',
-    '(?P<del_cost_wvat>(?<=€\n)[\d\,]+)[\s\S]+ODE[\w\s\-\/]+[\W]{3}',
-    '(?P<ode_novat>(?<=€\n)[\d\,]+)[\W]{3}',
-    '(?P<ode_wvat>(?<=€\n)[\d\,]+)\nVermindering energiebelasting[\w\s\-\/]+[\W]{3}',
-    '(?P<reduc_novat>(?<=€\n)[\-\d\,]+)[\W]{3}',
-    '(?P<reduc_wvat>(?<=€\n)[\-\d\,]+)\nNetbeheerkosten[\w\s\-\/]+[\W]{2}',
-    '(?P<nmcost_novat>(?<=€\n)[\d\,]+)[\W]{3}',
-    '(?P<nmcost_wvat>(?<=€\n)[\-\d\,]+)[\s\S]+verbruik[\w\s\-\/]+\n',
-    '(?P<gas_consumption>\d+)[\W]{3}m[\s\S]{5}',
-    '(?P<gas_cons_ppu>(?<=€\n)[\-\d\,]+)\W{3}',
-    '(?P<gas_cons_novat>(?<=€\n)[\d\,]+)\W{3}',
-    '(?P<gas_cons_wvat>(?<=€\n)[\d\,]+)[\s\S]+compensatie[\w\s\-\/]+\n',
-    '(?P<gas_compensation>\d+)[\W]{3}m[\s\S]{5}',
-    '(?P<gas_comp_ppu>(?<=€\n)[\-\d\,]+)\W{3}',
-    '(?P<gas_comp_novat>(?<=€\n)[\-\d\,]+)\W{3}',
-    '(?P<gas_comp_wvat>(?<=€\n)[\-\d\,]+)\nLeveringsadres:\n',
-    '(?P<delivery_add>[\w \,\d]+)[\s\S]+',
-    '(?P<incl_BWT>(?<=Incl\.[\W]{2})[\d\,]+)[\W]{2}BTW[\W]{3}',
-    '(?P<invoice_amt>(?<=€\n)[\d\,]+)[\s\S]+verbruik[\w\s\-\/]+\n',
-    '(?P<el_consumption>\d+)[\W]{3}kWh[\W]{3}',
-    '(?P<el_cons_ppu>(?<=€\n)[\-\d\,]+)[\W]{3}',
-    '(?P<el_cons_novat>(?<=€\n)[\d\,]+)[\W]{3}',
-    '(?P<el_cons_wvat>(?<=€\n)[\d\,]+)[\s\S]+Dekking extra maand\n',
-    '(?P<cover_extra_period>[\d]{2}\-[\d]{2} t\/m [\d]{2}\-[\d]{2})[\W]{3}',
-    '(?P<cover_extra_novat>(?<=€\n)[\d\,]+)[\W]{3}',
-    '(?P<cover_extra_wvat>(?<=€\n)[\d\,]+)',
-]
+DOCS_ROOT = '../docs/'
+GCREDS = './creds.json'
+GSPREADSHEET = 'Utilities Dashboard'
+EAN_ELECTRIC = r'(?P<EAN_el>(?<=\nEAN[\W]{2}elektriciteit\n:\n)[\d]+)'
+EAN_GAS = r'(?P<EAN_gas>(?<=\nEAN[\W]{2}gas\n:\n)[\d]+)'
+COVER_EXTRA = r'Dekking extra maand\n' \
+              r'(?P<cover_extra_period>[\d]{2}\-[\d]{2} t\/m [\d]{2}\-[\d]{2})[\W]{3}' \
+              r'(?P<cover_extra_novat>(?<=€\n)[\d\,]+)[\W]{3}' \
+              r'(?P<cover_extra_wvat>(?<=€\n)[\d\,]+)'
+REFUND_EXTRA = r'Restitutie extra maand\n' \
+              r'(?P<refund_extra_period>[\d]{2}\-[\d]{2} t\/m [\d]{2}\-[\d]{2})[\W]{3}' \
+              r'(?P<refund_extra_novat>(?<=€\n)[\d\,]+)[\W]{3}' \
+              r'(?P<refund_extra_wvat>(?<=€\n)[\d\,]+)'
+BILL_QUERY = r'Klantnummer:\n' \
+    r'(?P<customer_num>\d+)\nFactuurnummer:\n' \
+    r'(?P<invoice_num>\d+)\nVervaldatum:\n' \
+    r'(?P<due_date>[\d\-]+)[\s\S]+Termijnfactuur\s{3}' \
+    r'(?P<month>\w*)[\s\S]+Vaste leveringskosten\n' \
+    r'(?P<present_period>[\d]{2}\-[\d]{2} t\/m [\d]{2}\-[\d]{2})[\W]{3}' \
+    r'(?P<del_cost_novat>(?<=€\n)[\d\,]+)[\W]{3}' \
+    r'(?P<del_cost_wvat>(?<=€\n)[\d\,]+)[\s\S]+ODE[\w\s\-\/]+[\W]{3}' \
+    r'(?P<ode_novat>(?<=€\n)[\d\,]+)[\W]{3}' \
+    r'(?P<ode_wvat>(?<=€\n)[\d\,]+)\nVermindering energiebelasting[\w\s\-\/]+[\W]{3}' \
+    r'(?P<reduc_novat>(?<=€\n)[\-\d\,]+)[\W]{3}' \
+    r'(?P<reduc_wvat>(?<=€\n)[\-\d\,]+)\nNetbeheerkosten[\w\s\-\/]+[\W]{2}' \
+    r'(?P<nmcost_novat>(?<=€\n)[\d\,]+)[\W]{3}' \
+    r'(?P<nmcost_wvat>(?<=€\n)[\-\d\,]+)[\s\S]+verbruik[\w\s\-\/]+\n' \
+    r'(?P<gas_consumption>\d+)[\W]{3}m[\s\S]{5}' \
+    r'(?P<gas_cons_ppu>(?<=€\n)[\-\d\,]+)\W{3}' \
+    r'(?P<gas_cons_novat>(?<=€\n)[\d\,]+)\W{3}' \
+    r'(?P<gas_cons_wvat>(?<=€\n)[\d\,]+)[\s\S]+compensatie[\w\s\-\/]+\n' \
+    r'(?P<gas_compensation>\d+)[\W]{3}m[\s\S]{5}' \
+    r'(?P<gas_comp_ppu>(?<=€\n)[\-\d\,]+)\W{3}' \
+    r'(?P<gas_comp_novat>(?<=€\n)[\-\d\,]+)\W{3}' \
+    r'(?P<gas_comp_wvat>(?<=€\n)[\-\d\,]+)\nLeveringsadres:\n' \
+    r'(?P<delivery_add>[\w \,\d]+)[\s\S]+' \
+    r'(?P<incl_BWT>(?<=Incl\.[\W]{2})[\d\,]+)[\W]{2}BTW[\W]{3}' \
+    r'(?P<invoice_amt>(?<=€\n)[\d\,]+)[\s\S]+verbruik[\w\s\-\/]+\n' \
+    r'(?P<el_consumption>\d+)[\W]{3}kWh[\W]{3}' \
+    r'(?P<el_cons_ppu>(?<=€\n)[\-\d\,]+)[\W]{3}' \
+    r'(?P<el_cons_novat>(?<=€\n)[\d\,]+)[\W]{3}' \
+    r'(?P<el_cons_wvat>(?<=€\n)[\d\,]+)[\s\S]+[\s\S]+0\n\%[\W]{3}' \
+    r'(?P<vat_0>(?<=€\n)[\-\d\,]+)[\s\S]+9\n\%[\W]{3}' \
+    r'(?P<vat_9>(?<=€\n)[\-\d\,]+)[\s\S]+21\n\%[\W]{3}' \
+    r'(?P<vat_21>(?<=€\n)[\-\d\,]+)'
